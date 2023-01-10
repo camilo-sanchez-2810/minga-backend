63d9719e708eefa4521c6c1bc5caedf4cc64c2fb
LucianaRovere <luciana.rovere96@gmail.com>
2023-01-10 11:37:06 -0300

    paginacion y ordenamiento corregido

--- controllers/comic.all.controller.js
+++ controllers/comic.all.controller.js
@@ -1,8 +1,5 @@
-//1ro obtener todos los comics
-//2do crear los filtros
 import { Comic } from "../models/Comic.js";
 import defaultResponse from "../config/response.js";
-
 
 const controller = {
     read: async (req, res, next) => {
@@ -11,26 +8,40 @@ const controller = {
 		//REQ.PARAMS
 		//REQ.QUERY
         console.log(req.query)
-        let consultasParaFiltrar = {}
-        let ordenamiento = {}
-        if(req.query.title){
-            consultasParaFiltrar.title = req.query.title.split(',')
-            consultasParaFiltrar.title = { "$regex": req.query.title, $options: "i" };
+        let queriesToFilter = {}
+        let ordering = {}
+        let pagination = {
+            page:1 ,
+            limit: 10 
+            
         }
-
-        
+        if(req.query.title){
+            queriesToFilter.title = req.query.title.split(',')
+            queriesToFilter.title = { "$regex": req.query.title, $options: "i" };
+        }
         if (req.query.category_id){
-            consultasParaFiltrar.category_id = req.query.category_id
+            queriesToFilter.category_id = req.query.category_id
 
         }
         if (req.query.sort){
-            ordenamiento = {title: "-1"}
+            
+ordering = {title: req.query.sort}
         }
-        
 
+        if (req.query.page) {
+			pagination.page = req.query.page;
+		}
+		if (req.query.limit) {
+			pagination.limit = req.query.limit;
+		}
 
     try {
-        let all = await Comic.find(consultasParaFiltrar)
+        let all = await Comic.find(queriesToFilter)
+        .sort(
+ordering)
+        .skip( pagination.page > 0 ? (pagination.page - 1) * pagination.limit : 0
+        )
+        .limit(pagination.limit)
             
         if (all) {
             req.body.success = true;
