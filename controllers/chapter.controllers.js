340f236fd94c4219b2dd6ca6ff0a552a5fc6f4d3
laptinho <camsanbar@gmail.com>
2023-01-10 18:30:21 -0300

    Fix: cambiado a req.params & sort del array de paginas

--- controllers/chapter.controllers.js
+++ controllers/chapter.controllers.js
@@ -1,43 +1,50 @@
-import {Chapter} from '../models/Chapter.js'
-import defaultResponse from '../config/response.js'
+import { Chapter } from "../models/Chapter.js";
+import defaultResponse from "../config/response.js";
 
 const controller = {
-    create: async(req, res)=>{
-        try{
-            const {comic_id,title, pages,order} = req.body
-            await Chapter.create({comic_id,title, pages,order}) //el usuario no envia order
-            req.body.success = true;
+	create: async (req, res) => {
+		try {
+			const { comic_id, title, pages, order } = req.body;
+			await Chapter.create({ comic_id, title, pages, order }); //el usuario no envia order
+			req.body.success = true;
 			req.body.sc = 201;
-			req.body.data = 'chapter created';
+			req.body.data = "chapter created";
 			return defaultResponse(req, res);
-        } catch(error){
-            req.body.success = false;
+		} catch (error) {
+			req.body.success = false;
 			req.body.sc = 500;
-			req.body.data = 'error';
+			req.body.data = "error";
 			return defaultResponse(req, res);
-        }
-    },
-    get_pages: async (req, res) => {
-        const id = req.path.replace('/','')
-        try {
-            const chapter = await Chapter.findById(id)
-            if (chapter) {
-                req.body.success = true;
-                req.body.sc = 200;
-                req.body.data = chapter;
-                return defaultResponse(req, res);
-            } else {
-                req.body.success = false;
-                req.body.sc = 404;
-                req.body.data = 'chapter not found';
-                return defaultResponse(req, res);
-            }
-        } catch(error) {
-            req.body.success = false;
+		}
+	},
+	get_pages: async (req, res) => {
+		const { id } = req.params;
+		try {
+			const chapter = await Chapter.findById(id,"title pages order");
+			const dataResponse = {
+				title: chapter.title,
+				order: chapter.order,
+				pages: chapter.pages.sort((firstElement, secondElement) =>
+					secondElement.localeCompare(firstElement)
+				),
+			};
+			if (chapter) {
+				req.body.success = true;
+				req.body.sc = 200;
+				req.body.data = dataResponse;
+				return defaultResponse(req, res);
+			} else {
+				req.body.success = false;
+				req.body.sc = 404;
+				req.body.data = "chapter not found";
+				return defaultResponse(req, res);
+			}
+		} catch (error) {
+			req.body.success = false;
 			req.body.sc = 500;
-			req.body.data = 'error';
+			req.body.data = "error";
 			return defaultResponse(req, res);
-        }
-    }
-}
-export default controller
+		}
+	},
+};
+export default controller;
