7a4bb0ba3255d7aa073796bfb5e275eee854ded0
laptinho <camsanbar@gmail.com>
2023-01-19 12:25:40 -0300

    Fix: fix error en middleware isAuthorActive

--- middlewares/isAuthorActive.js
+++ middlewares/isAuthorActive.js
@@ -1,14 +1,16 @@
 import defaultResponse from '../config/response.js'
+import { Author } from '../models/Author.js'
 
 async function isAuthorActive(req, res, next) {
-    const { is_author } = req.user // El id del autor deberia pasarse de otra forma (Proximamente)
+    const { id } = req.user // El id del autor deberia pasarse de otra forma (Proximamente)
     // Primero buscamos el autor
     // Verificamos si esta activo
     //   Si esta activo lo dejo pasar al controlador
     //   Si no esta activo no lo dejo crear un comic (NO LO DEJO PASAR AL CONTROLADOR)
     try {
-      if (is_author) {
-        next()
+      const author = await Author.findById(id)
+      if (author.active) {
+        return next()
       }
       req.body.success = false
       req.body.sc = 400
